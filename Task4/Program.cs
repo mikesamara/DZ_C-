dce8480daf0d9f4f972eb628446c307a51b99cd0
Maik <samara.maik@yandex.by>
2023-01-18 18:56:48 +0300

    add task4

---
+++ Task4/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.Write("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+int count=1;
+while (count <= N){ 
+    if (count % 2 == 0) {
+    System.Console.WriteLine(count);}
+    count= count+1;
+}

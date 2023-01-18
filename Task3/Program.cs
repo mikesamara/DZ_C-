8ca8189348977c62a556483126bea7ff064bb727
Maik <samara.maik@yandex.by>
2023-01-18 18:42:05 +0300

    add task3

---
+++ Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.Write("Введите число а:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 ==0) {
+    System.Console.WriteLine("Число является четным");
+}
+else{
+    System.Console.WriteLine("Число является нечетным");
+} 

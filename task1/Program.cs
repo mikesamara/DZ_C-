7415d6a2a1ae7179861d05e6070d2c2eb4398a53
Maik <samara.maik@yandex.by>
2023-01-18 18:10:23 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.Write("Введите число а:");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число b:");
+int b = Convert.ToInt32(Console.ReadLine());
+int max=0;
+if (a>b) {
+    max = a;
+    System.Console.Write("Максимальное числа а:");
+    System.Console.Write(max);
+}
+else
+{
+    max = b;
+    System.Console.Write("Максимальное число b: ");
+    System.Console.Write(max);
+}

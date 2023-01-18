5abaae02c26fcf3e25c7a557ec2de365251ca625
Maik <samara.maik@yandex.by>
2023-01-18 18:32:24 +0300

    add task2

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿System.Console.Write("Введите число а:");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число b:");
+int b = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число b:");
+int c = Convert.ToInt32(Console.ReadLine());
+int max=0;
+if (a>b) { 
+    max = a;
+    }
+else { 
+    max = b;
+}
+if (max>c){
+    System.Console.Write("Максимальное число:");
+    System.Console.Write(max);
+}
+else {
+    max=c;
+    System.Console.Write("Максимальное числа:");
+    System.Console.Write(max);
+}
